Added a variance visualizer in Kalman Filter. Also solved bug in Kalman Filter that uses the predicted state as measurement if no measurement is available

scripts/kalmanFilter_position.py
@@ -12,7 +12,7 @@ class KalmanFilter:
 
 		# Variances used in prediction
 		self.initialStateVariance = initial_state_variance
-		self.processVariance = 1000
+		self.processVariance = 10**6
 		self.U = 1 if method == "Acceleration" else 0
 		
 		dt = 1/frequency
@@ -115,7 +115,7 @@ class KalmanFilter:
 		])
 
 		# Update observation matrix
-		self.Q = self.B * self.B.T
+		self.Q = self.B * np.diag(np.ones((1, 1))) * self.processVariance * self.B.T
 
 		
 

scripts/multiObjectTracker.py
@@ -11,6 +11,9 @@ from ahold_product_detection.msg import FloatList, ProductList
 from tf.transformations import quaternion_from_euler
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
+
+
+
 class rotatedRect():
 
     def __init__(self, frame, center, width, height, angle, color, thickness) -> None:
@@ -49,8 +52,6 @@ class rotatedRect():
 class Tracks:
 
     def __init__(self, measurement, classification, score, track_id, frequency):
-        #TODO: implement if
-        #state = np.array([measurement[0], 0, measurement[1], 0, measurement[2], 0, measurement[3], 0, measurement[4], 0, measurement[5], 0]) # initial state, speeds are both 0
         state = np.array(measurement) # initial state, speeds are both 0
         self.KF = KalmanFilter(init_state=state, frequency=frequency, measurement_variance=0.1)
         self.trace = deque(maxlen=20)
@@ -58,6 +59,7 @@ class Tracks:
         self.skipped_frames = 0
         self.classifications = []
         self.scores = []
+        self.frequencies = []
         if classification == classification and score == score:
             self.classifications.append(classification)
             self.scores.append(score)
@@ -65,8 +67,6 @@ class Tracks:
 
     @property
     def prediction(self):
-        #TODO: implement if
-        #return np.array([self.KF.pred_state[0, 0], self.KF.pred_state[2, 0], self.KF.pred_state[4, 0], self.KF.pred_state[6, 0], self.KF.pred_state[8, 0], self.KF.pred_state[10, 0]])
         return np.array([self.KF.pred_state[0, 0], self.KF.pred_state[1, 0], self.KF.pred_state[2, 0], self.KF.pred_state[3, 0], self.KF.pred_state[4, 0], self.KF.pred_state[5, 0]])
 
 
@@ -89,6 +89,11 @@ class Tracks:
         return mean_score
 
     
+    @property
+    def variance(self):
+        return self.KF.pred_err_cov
+
+
 
     def update(self, measurement, classification, score, frequency):
         # Add classification to classifications
@@ -97,11 +102,21 @@ class Tracks:
             self.scores.append(score)
 
         # Update the state transition matrix based on the passed time
-        self.KF.update_matrices(frequency)
+        if self.frequencies == []:
+            self.KF.update_matrices(frequency)
+        else:
+            freq = 1/(sum([1/f for f in self.frequencies]) + 1/frequency)
+            self.KF.update_matrices(freq)
 
         # Update the state based on the new measurements
         self.KF.update(np.array(measurement).reshape(6, 1))
-        
+
+
+
+    def update_no_measurement(self, frequency):
+        self.frequencies.append(frequency)
+        freq = 1/sum([1/f for f in self.frequencies])
+        self.KF.update_matrices(freq)
 
 
 class Tracker:
@@ -133,21 +148,21 @@ class Tracker:
 
         
 
-        if (len(detected_products) > 0): 
+        
 
-            current_time = rospy.get_time()
-            if self.previous_measurement_exists:
-                delta_t = current_time - self.prev_time
-            else:
-                delta_t = 1
-            
-            self.previous_measurement_exists = True
-            self.prev_time = current_time
-            self.update(product_poses, product_classes, product_scores, 1/float(delta_t)) 
-            product_to_grasp = self.choose_desired_product()
-            if product_to_grasp != None:
-                self.broadcast_product_to_grasp(product_to_grasp)
-            self.visualize(product_poses, product_to_grasp)
+        current_time = rospy.get_time()
+        if self.previous_measurement_exists:
+            delta_t = current_time - self.prev_time
+        else:
+            delta_t = 1
+        
+        self.previous_measurement_exists = True
+        self.prev_time = current_time
+        self.update(product_poses, product_classes, product_scores, 1/float(delta_t)) 
+        product_to_grasp = self.choose_desired_product()
+        if product_to_grasp != None:
+            self.broadcast_product_to_grasp(product_to_grasp)
+        self.visualize(product_poses, product_to_grasp)
 
 
 
@@ -174,17 +189,13 @@ class Tracker:
         t.transform.rotation.z = q[2]
         t.transform.rotation.w = q[3]
 
-        print(t)
         br.sendTransform(t)
 
 
         
     def visualize(self, measurements, product_to_grasp):
-        frame_xz = self.draw_xz_view(measurements, product_to_grasp)
-        #frame_yz = self.draw_xy_view(measurements)
-        
+        frame_xz = self.draw_xz_view(measurements, product_to_grasp)        
         cv2.imshow('kalman filter xz', frame_xz)
-        #cv2.imshow('kalman filter xy', frame_yz)
         cv2.waitKey(1)
 
 
@@ -208,18 +219,16 @@ class Tracker:
         # Draw the latest updated states
         for track in self.tracks:
             updated_state = track.trace[-1]
-            
-            """
-            x = -int(scale * updated_state[0]) + int(width/2)
-            z = int(scale *updated_state[4])
-            """
 
             x = -int(scale * updated_state[0]) + int(width/2)
             z = int(scale *updated_state[2])
             theta = updated_state[4][0]
 
+            variance_scale = 100000
+            axis_length = (int(track.KF.pred_err_cov[0,0]/variance_scale), int(track.KF.pred_err_cov[2,2]/variance_scale))
+            print(axis_length)
             rotatedRect(frame, (x, z), 20, 20, theta, (0,0,255), 3)
-            
+            cv2.ellipse(frame, (x, z), axis_length, 0, 0, 360, (0,255,255), 3)
             cv2.putText(frame, str(track.classification), (x + 5, z - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (0,200,0), 1)
         
         # product to grasp
@@ -282,7 +291,6 @@ class Tracker:
             if self.index_product_to_grasp == None:
                 self.initial_score_product_to_grasp = max(detected_desired_product_scores)
                 self.index_product_to_grasp = self.tracks[detected_desired_product_scores.index(self.initial_score_product_to_grasp)].track_id
-                print('assigned product to grasp')
             
         desired_track = []
         if self.index_product_to_grasp != None:
@@ -321,7 +329,7 @@ class Tracker:
         
         print(np.round((10**9)*self.measurement_variance, decimals=1))
         print(self.num_measurements)
-
+    
 
 
     def update(self, measurements, classifications, scores, current_frequency):
@@ -332,39 +340,52 @@ class Tracker:
                 self.tracks.append(Tracks(measurement, classifications[i], scores[i], self.current_track_id, current_frequency))
                 self.current_track_id += 1
         
-        # Calculate distance measurements w.r.t. existing track predictions
-        dists = np.array([np.linalg.norm(measurements - track.prediction, axis=1) for track in self.tracks])
+        if len(measurements) > 0:
+            # Calculate distance measurements w.r.t. existing track predictions
+            dists = np.array([np.linalg.norm(measurements - track.prediction, axis=1) for track in self.tracks])
 
-        # Determine which measurement belongs to which track
-        assignment = np.array(linear_sum_assignment(dists)).T
-        
-        # Only assign a measurement to a track if it is close enough to the predicted position
-        assignment = [a for a in assignment if dists[a[0], a[1]] < self.dist_threshold]
+            # Determine which measurement belongs to which track
+            assignment = np.array(linear_sum_assignment(dists)).T
+            
+            # Only assign a measurement to a track if it is close enough to the predicted position
+            assignment = [a for a in assignment if dists[a[0], a[1]] < self.dist_threshold]
 
-        # Update state of existing tracks with measurement
-        for track_idx, measurement_idx in assignment:
-            self.tracks[track_idx].update(measurements[measurement_idx], classifications[measurement_idx], scores[measurement_idx], current_frequency)
-            self.tracks[track_idx].skipped_frames = 0
-   
-            #self.calculate_variance_measurements(measurements[measurement_idx])
+            # Update state of existing tracks with measurement
+            for track_idx, measurement_idx in assignment:
+                self.tracks[track_idx].update(measurements[measurement_idx], classifications[measurement_idx], scores[measurement_idx], current_frequency)
+                self.tracks[track_idx].skipped_frames = 0
+                self.tracks[track_idx].frequencies = []
+    
+                #self.calculate_variance_measurements(measurements[measurement_idx])
+            
+            # Create new tracks for measurements without track
+            assigned_det_idxs = [det_idx for _, det_idx in assignment]
+            for i, det in enumerate(measurements):
+                if i not in assigned_det_idxs:
+                    # TODO: do not add tracks that are (0, 0, 0) (bad measurement)
+                    self.tracks.append(Tracks(det, classifications[i], scores[i], self.current_track_id, current_frequency))
+                    self.current_track_id += 1
 
-        # Propagate unassigned tracks using the prediction 
-        assigned_track_idxs = [track_idx for track_idx, _ in assignment]
-        for i, track in enumerate(self.tracks):
-            if i not in assigned_track_idxs:
+            # Propagate unassigned tracks using the prediction 
+            assigned_track_idxs = [track_idx for track_idx, _ in assignment]
+            for i, track in enumerate(self.tracks):
+                if i not in assigned_track_idxs:
+                    # No measurement available, assume prediction is right
+                    track.update_no_measurement(current_frequency)
+
+                    # Keep track of the missed measurements of this object
+                    track.skipped_frames += 1
+
+        else:
+            # No measurements, update tracks according to prediction
+            for i, track in enumerate(self.tracks):
                 # No measurement available, assume prediction is right
-                track.update(track.prediction, track.classification, track.score, current_frequency)
+                #TODO: do not update
+                track.update_no_measurement(current_frequency)
 
                 # Keep track of the missed measurements of this object
                 track.skipped_frames += 1
 
-        # Create new tracks for measurements without track
-        assigned_det_idxs = [det_idx for _, det_idx in assignment]
-        for i, det in enumerate(measurements):
-            if i not in assigned_det_idxs:
-                # TODO: do not add tracks that are (0, 0, 0) (bad measurement)
-                self.tracks.append(Tracks(det, classifications[i], scores[i], self.current_track_id, current_frequency))
-                self.current_track_id += 1
 
         # Delete tracks if skipped_frames too large
         self.tracks = [track for track in self.tracks if not track.skipped_frames > self.max_frame_skipped]
@@ -385,16 +406,3 @@ def createimage(w,h):
 	img = np.ones((w,h,3),np.uint8)*255
 	return img
 
-
-
-def main():
-    dist_threshold = 2 # if objects are standing still, choose 0.1 (=10 cm), if movement, choose 0.5
-    max_frame_skipped = 5
-    max_trace_length = 3
-    Tracker(dist_threshold=dist_threshold, max_frame_skipped=max_frame_skipped, max_trace_length=max_trace_length, frequency=1)
-
-
-
-
-if __name__ == '__main__':
-	main()

scripts/predict.py
@@ -10,7 +10,7 @@ from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs import point_cloud2
 from std_msgs.msg import Header, Int32
 from ahold_product_detection.msg import FloatList, PointCloudList, ProductClass, ProductList
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import tf2_ros
 import tf
 from geometry_msgs.msg import TransformStamped, PoseStamped
@@ -231,7 +231,46 @@ def read_message(message):
 
 
 
-def estimate_pose_object(depth_image, depth_bounding_box, intrinsic_camera_matrix, frame_id, name, score):
+def convert_pose(pose, frame, listener):
+    x, y, z, theta, phi, psi = pose
+    
+    # make tf PoseStamped for transformation
+    p = PoseStamped()
+    p.header.stamp = rospy.Time.now()
+    p.header.frame_id = "camera_color_optical_frame"
+    p.pose.position.x = x
+    p.pose.position.y = y
+    p.pose.position.z = z
+    q = quaternion_from_euler(theta, phi, 0)
+    p.pose.orientation.x = q[0]
+    p.pose.orientation.y = q[1]
+    p.pose.orientation.z = q[2]
+    p.pose.orientation.w = q[3]
+
+    # Transform pose to desired frame
+    p_t = listener.transformPose(frame, p)
+    
+    # Convert back to original format
+    q_t = [p_t.pose.orientation.x,
+           p_t.pose.orientation.y,
+           p_t.pose.orientation.z,
+           p_t.pose.orientation.w]
+    
+    euler = euler_from_quaternion(q_t)
+
+    new_pose = [p_t.pose.position.x,
+                p_t.pose.position.y, 
+                p_t.pose.position.z, 
+                euler[0],
+                euler[1],
+                euler[2]]
+    
+    rospy.loginfo("Converted pose from 'camera_color_optical_frame' to " + frame)
+    return new_pose
+
+
+
+def estimate_pose_object(depth_image, depth_bounding_box, intrinsic_camera_matrix, frame_id, name, score, listener):
     """Returns pose message and pointcloud message"""
     # Calculate middle of bounding box (which is the center of the object's surface)
     xyz_vector = get_grasp_coordinates(depth_image, depth_bounding_box, intrinsic_camera_matrix)
@@ -242,17 +281,29 @@ def estimate_pose_object(depth_image, depth_bounding_box, intrinsic_camera_matri
     # Estimate orientation of object using a plane fit around the object's surface pointcloud
     orientation = estimate_pointcloud_orientation_with_plane(pointcloud)
     
+    pose = xyz_vector.tolist() + orientation
+
+    robot = False
+
+    if robot:
+        frame = 'base_link'
+    else:
+        frame = 'camera_color_optical_frame'
+    
+    pose = convert_pose(pose, frame, listener)
+    vector = [name] + [score] + pose
+
     pose_message = FloatList()
-    vector = [name] + [score] + xyz_vector.tolist() + orientation
     pose_message.data = vector
 
     return pose_message, pointcloud_message
 
 
 
-def estimate_pose_detected_objects(rgb_image, depth_image, rgb_bounding_boxes, intrinsic_camera_matrix, frame_id, names, scores):
+def estimate_pose_detected_objects(rgb_image, depth_image, rgb_bounding_boxes, intrinsic_camera_matrix, frame_id, names, scores, listener):
     """Returns the poses and pointclouds of all detected objects"""
     pointcloud_messages = []
+    products = ProductList()
     object_poses = []
     
     # Relate rgb_bounding_boxes to depth image
@@ -266,174 +317,60 @@ def estimate_pose_detected_objects(rgb_image, depth_image, rgb_bounding_boxes, i
                                                                 intrinsic_camera_matrix, 
                                                                 frame_id,
                                                                 name,
-                                                                score)
+                                                                score,
+                                                                listener)
         
         object_poses.append(pose_message)
         pointcloud_messages.append(pointcloud_message)
-    
-    return object_poses, pointcloud_messages
+    products.data = object_poses
+    return products, pointcloud_messages
 
 
 
-def find_desired_object(desired_object_index, names, object_poses, scores, pointcloud_messages):
-    """Returns all objects that are desired, along with their scores and pointclouds"""
-    desired_objects = []
-    desired_object_scores = []
-    desired_pointclouds = []
-
-
-    for i, name in enumerate(names):
-        if name == desired_object_index:
-            
-            desired_objects.append(object_poses[i])
-            desired_object_scores.append(scores[i])
-            desired_pointclouds.append(pointcloud_messages[i])
-    
-    return desired_objects, desired_object_scores, desired_pointclouds
-
-
-
-def broadcast_tf_transform_of_object(object_pose, index, timeStamp, listener):
-    """Converts message to a tf2 frame when message becomes available, and then broadcasts it"""
-    br = tf2_ros.TransformBroadcaster()
-    
-
-    x, y, z, theta, phi, psi = object_pose.data
-    
-    robot = False
-    if robot:
-        p = PoseStamped()
-        p.header.stamp = timeStamp
-        rospy.logwarn(timeStamp)
-        p.header.frame_id = "camera_color_optical_frame"
-        #t.child_frame_id = 'object'+str(index)
-        p.pose.position.x = x
-        p.pose.position.y = y
-        p.pose.position.z = z
-        q = quaternion_from_euler(theta, phi, 0)
-        p.pose.orientation.x = q[0]
-        p.pose.orientation.y = q[1]
-        p.pose.orientation.z = q[2]
-        p.pose.orientation.w = q[3]
-
-        p_transformed = listener.transformPose('base_link', p)
-
-        t = TransformStamped()
-
-        t.header.stamp = timeStamp
-        t.header.frame_id = 'base_link'
-        t.child_frame_id = 'object'+str(index)
-        t.transform.translation.x = p_transformed.pose.position.x
-        t.transform.translation.y = p_transformed.pose.position.y
-        t.transform.translation.z = p_transformed.pose.position.z
-        t.transform.rotation.x = p_transformed.pose.orientation.x
-        t.transform.rotation.y = p_transformed.pose.orientation.y
-        t.transform.rotation.z = p_transformed.pose.orientation.z
-        t.transform.rotation.w = p_transformed.pose.orientation.w
-        
-    else:
-        t = TransformStamped()
-        t.header.stamp = timeStamp
-        t.header.frame_id = 'camera_color_optical_frame'
-        t.child_frame_id = 'object'+str(index)
-        t.transform.translation.x = x
-        t.transform.translation.y = y
-        t.transform.translation.z = z
-        q = quaternion_from_euler(theta, phi, 0)
-        t.transform.rotation.x = q[0]
-        t.transform.rotation.y = q[1]
-        t.transform.rotation.z = q[2]
-        t.transform.rotation.w = q[3]
-    br.sendTransform(t)
-
-
-
-def calculate_distance_poses(pose1, pose2):
-    return np.sqrt((pose1[0] - pose2[0])**2 + (pose1[1] - pose2[1])**2 + (pose1[2] - pose2[2])**2)
-
-
-
-def search_products(message, model):
+def search_products(message, model, listener):
     # Convert message to usable data
     rgb_image, depth_image, intrinsic_camera_matrix, frame_id = read_message(message)
 
     # Predict product bounding boxes and classes
     rgb_bounding_boxes, names, scores = predict(model, rgb_image)
     
-    object_poses, pointcloud_messages = estimate_pose_detected_objects(rgb_image, 
+    products, pointcloud_messages = estimate_pose_detected_objects(rgb_image, 
                                                                     depth_image, 
                                                                     rgb_bounding_boxes, 
                                                                     intrinsic_camera_matrix, 
                                                                     frame_id,
                                                                     names, 
-                                                                    scores)
-    return object_poses, pointcloud_messages, names, scores
+                                                                    scores,
+                                                                    listener)
+    return products, pointcloud_messages 
 
 
-
-def broadcast_detected_products(object_poses, names, scores, pub_object_class, listener, num_objects):
-    if len(object_poses) > num_objects:
-        new_num_objects = len(object_poses)
-    else:
-        new_num_objects = num_objects
-
-    for i in range(new_num_objects):
-
-        try:
-            object_pose = object_poses[i]
-            timeStamp = rospy.Time.now()
-            
-            object_class_message = ProductClass()
-            object_class_message.classification = int(names[i])
-            object_class_message.score = scores[i]
-            object_class_message.header.stamp = timeStamp
-            pub_object_class.publish(object_class_message)
-            broadcast_tf_transform_of_object(object_pose, i, timeStamp, listener)
-
-        except:
-            empty_pose = FloatList()
-            empty_pose.data = [0, 0, 0, 0, 0, 0]
-            broadcast_tf_transform_of_object(empty_pose, i, rospy.Time.now(), listener)
-
-    return new_num_objects
     
-
-
 def main():
     # Load yolo model weights for detection
     weight_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'yolo_model', 'nano_supermarket_best.pt')
     model = ultralytics.YOLO(weight_path)
 
-    # Initialize publishers for intermediate results visualization
-    pub_num_objects = rospy.Publisher("number_of_objects", Int32, queue_size=1)
-    pub_object_class = rospy.Publisher("detected_classes", ProductClass, queue_size=1)
-
+    # Initialize ros node
     rospy.init_node('Product_detector', anonymous=False)
-
     listener = tf.TransformListener()
     rospy.sleep(1)
-    num_objects = 0
 
-    # init kalman filter
-    dist_threshold = 2 # if objects are standing still, choose 0.1 (=10 cm), if movement, choose 0.5
-    max_frame_skipped = 5
+    # Initialize kalman filter for object tracking
+    dist_threshold = 0.1 # if objects are standing still, choose 0.1 (=10 cm), if movement, choose 0.5
+    max_frame_skipped = 15
     max_trace_length = 3
     tracker = Tracker(dist_threshold=dist_threshold, max_frame_skipped=max_frame_skipped, max_trace_length=max_trace_length, frequency=1)
 
     while not rospy.is_shutdown():
-        # Receive message from rgbd_processor
+        # Request new message from rgbd_processor
         message = requestRGBD_client()
         
-        rospy.loginfo('recieved message')
         if message != None:
-            object_poses, pointcloud_messages, names, scores = search_products(message, model)
+            # Detect, localize and transform the detected products
+            products, pointcloud_messages = search_products(message, model, listener)                       
             
-            num_objects = broadcast_detected_products(object_poses, names, scores, pub_object_class, listener, num_objects)
-                
-            pub_num_objects.publish(num_objects)
-            
-            products = ProductList()
-            products.data = object_poses
+            # Track the detected products with Kalman Filter
             tracker.process_detections(products)
             
 